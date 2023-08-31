1d45247b6e44a53fb11831d42ddcc4f8bb8cff14
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-08-31 10:42:29 +0500

    fix: make time zone of program consistent
    see time zone of rx_poc.sh

--- fc-compare.sh
+++ fc-compare.sh
@@ -1,4 +1,4 @@
 !#/bin/bash
-tdDate=$(date +%Y%m%d)
+current_time=$(TZ='Morocco/Casablanca' date)
 fcTemp=$(tail -2 rx_poc.log | head -1 | cut -d " " -f6)
 acTemp=$(tail -1 rx_poc.log | cut -d " " -f5)

5271e91bb651def08fe5a36bbddaffbb5d48f3e7
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-08-31 21:38:59 +0500

    feat: compute value of accurace range label

--- fc-compare.sh
+++ fc-compare.sh
@@ -6,3 +6,13 @@ year=$(echo $current_time | cut -d " " -f3)
 tempDiff=$(($fcTemp-$acTemp))
 fcTemp=$(tail -2 rx_poc.log | head -1 | cut -d " " -f6)
 acTemp=$(tail -1 rx_poc.log | cut -d " " -f5)
+accuracyRangeLabel=""
+if (( $(echo "$tempDiff<=1.0") | bc )) && (( $(echo "$tempDiff>=-1.0" | bc) )) ; then
+accuracyRangeLabel="excellent"
+elif (( $(echo "$tempDiff<=2.0" | bc) )) && (( $(echo "$tempDiff>=-2.0" | bc) )) ; then
+accuracyRangeLabel="good"
+elif (( $(echo "$tempDiff<=3.0" | bc) )) && (( $(echo "$tempDiff>=-3.0" | bc) )); then
+accuracyRangeLabel="fair"
+else
+accuracyRangeLabel="poor"
+fi

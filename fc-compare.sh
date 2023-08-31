9ca0dfaba99480384ce1d64c32d82218df0d93e4
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-08-31 10:43:33 +0500

    feat: make and assign variables for time data entries

--- fc-compare.sh
+++ fc-compare.sh
@@ -1,4 +1,8 @@
 !#/bin/bash
 current_time=$(TZ='Morocco/Casablanca' date)
+day=$(echo $current_time | cut -d " " -f5)
+month=$(echo $current_time | cut -d " " -f4)
+year=$(echo $current_time | cut -d " " -f3)
+tempDiff=$(($fcTemp-$acTemp))
 fcTemp=$(tail -2 rx_poc.log | head -1 | cut -d " " -f6)
 acTemp=$(tail -1 rx_poc.log | cut -d " " -f5)

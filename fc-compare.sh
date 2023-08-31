b92e29975487ba3b4150f555a1a31fb5af4346cb
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-08-31 22:34:02 +0500

    fix: separate code of programs into 2 files
    one file has the capability to run on schedule, adding predications of one day at a time, while the other (fc-compare-on-demand.sh) has the ability to generate predicaiton accuracies of all available weather data on user demand

--- fc-compare.sh
+++ fc-compare.sh
@@ -1,33 +1,4 @@
 !#/bin/bash
-no_of_lines=$( $(wc -l rx_poc.log | cut -d " " -f1 )
-touch historical_fc_accuracy.tsv
-echo "year\tmonth\tday\tobs_tmp\tfc_temp\taccuracy\taccuracy_range" > historical_fc_accuracy.tsv
-for (( i=$(( no_of_lines-1 )); i>2; i-- )) ; do
-current=$(( $no_of_lines-$i ))
-row=$(tail -$current rx_poc.log)
-day=$(echo $row | cut -d " " -f3)
-month=$(echo $row | cut -d " " -f2)
-year=$(echo $row | cut -d " " -f1)
-
-fcTemp=$(tail -$(( $current+1 )) rx_poc.log | head -1 | cut -d " " -f6)
-acTemp=$(tail -$current rx_poc.log | cut -d " " -f5)
-tempDiff=$(($fcTemp-$acTemp))
-accuracyRangeLabel=""
-if (( $(echo "$tempDiff<=1.0") | bc )) && (( $(echo "$tempDiff>=-1.0" | bc) )) ; then
-accuracyRangeLabel="excellent"
-elif (( $(echo "$tempDiff<=2.0" | bc) )) && (( $(echo "$tempDiff>=-2.0" | bc) )) ; then
-accuracyRangeLabel="good"
-elif (( $(echo "$tempDiff<=3.0" | bc) )) && (( $(echo "$tempDiff>=-3.0" | bc) )); then
-accuracyRangeLabel="fair"
-else
-accuracyRangeLabel="poor"
-fi
-echo "$year\t$month\t$day\t$acTemp\t$tempDiff\t$accuracyRangeLabel" >> ./historical_fc_accuracy.tsv
-done
-
-
-
-
 row=$(tail -1 rx_poc.log)
 day=$(echo $row | cut -d " " -f3)
 month=$(echo $row | cut -d " " -f2)
